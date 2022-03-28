3064a682863e8d7b457f72b61cfc9d25c8859f16
Elad Sapir <elad.1999.1999@gmail.com>
2022-03-28 22:37:39 +0300

    finished - FourTree

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import FourTreeCLS
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# the main menu start here, loop that asks the user to do what he wants according to the menu:
+mainTree = '0'
+print("Hello")
+while True:
+    dic = {1: FourTreeCLS.makeTree, 2: FourTreeCLS.addToTree, 3: FourTreeCLS.deleteNodeFromTree}
+    x = input("Menu:\n 1) Make new tree\n 2) Add to existing tree\n"
+              " 3) Delete from existing tree\n 4) Print the tree\n 5) Exit\n")
+    if x == '1':
+        mainTree = dic[1]()
+    elif x == '2':
+        try:
+            dic[2](mainTree)
+        except UnboundLocalError as err:
+            print(f'Unbound local error,not optional - {err}')
+    elif x == '3':
+        try:
+            dic[3](mainTree)
+        except UnboundLocalError as err:
+            print(f'Unbound local error,not optional - {err}')
+    elif x == '4':
+        if mainTree == '0':
+            print("The tree is empty.")
+        else:
+            print(mainTree)
+    elif x == '5':
+        print("Thanks, good bye")
+        break
+    else:
+        print("Wrong choose, try again please\n")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
